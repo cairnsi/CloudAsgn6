78371fa85e1f64ae023e16d67260e49d7772796f
cairnsi <59540148+cairnsi@users.noreply.github.com>
2021-05-06 20:23:27 -0700

    added state check

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 const path = require(`path`);
 const bodyParser = require('body-parser');
+const {Datastore} = require('@google-cloud/datastore');
+const datastore = new Datastore();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
@@ -11,10 +13,39 @@ app.set('view engine', 'handlebars');
 app.enable('trust proxy');
 var request = require('request');
 
-var redirect_uri= "http://localhost:8080/oauth";
+var redirect_uri= "https://assignment6-312700.wn.r.appspot.com/oauth";
 var client_id = "916034912784-vmi2ekt46gp1bac0svv4t3acj2rnn8oj.apps.googleusercontent.com";
 var scope = "https://www.googleapis.com/auth/userinfo.profile";
 client_secret = "81GIJ6ztg99K7LzYAsaX_nxo";
+const STATE_Key= "STATE";
+
+function fromDatastore(item){
+    item.id = item[Datastore.KEY].id;
+    return item;
+	
+}
+
+async function checkState(state){
+	const q = datastore.createQuery(STATE_Key);
+	entities = await datastore.runQuery(q);
+	states = entities[0];
+	states = states.map(fromDatastore);
+	foundState = false;
+	id = null;
+	states.forEach(function(ele) {
+		if(ele.state == state){
+			foundName = true;
+			id = ele.id;
+		}
+	});
+	if(foundName){
+		const key = datastore.key([STATE_Key, parseInt(id,10)]);
+		var [ele] = await datastore.get(key);
+		await datastore.delete(key);
+	}
+	return foundName;
+	
+}
 
 app.get('/',function(req,res){
   var context = {};
@@ -22,10 +53,10 @@ app.get('/',function(req,res){
 });
 
 app.get('/oauth',function(req,res){
-  if(req.query.code){
-	  console.log(req.query.code);
-  }else{
-	  console.log('error');
+  if(!checkState(req.query.state)){
+		error = {"Error": "The state value was not correct. "}
+		res.status(400).send(error);
+		return;
   }
    var context = {};
    
@@ -53,9 +84,12 @@ app.get('/oauth',function(req,res){
 	
 });
 
-app.post('/Authenticate',function(req,res){
+app.post('/Authenticate', async function(req,res){
 	
 	var state = "state" + Math.floor(Math.random() * 1000000); 
+	var key = datastore.key(STATE_Key);
+	const new_State = {"state": state};
+	await datastore.save({"key":key, "data":new_State});
 	
    res.writeHead(301,
   {Location: 'https://accounts.google.com/o/oauth2/v2/auth?client_id=' + client_id + '&scope=' + scope + '&redirect_uri=' + redirect_uri + '&state=' + state + '&response_type=code'}
